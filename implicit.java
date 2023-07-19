38ccbbabf720a42f2bf5c97a65091631614bdf6f
Sanket Kishor Boraste <sanketboraste162@gmail.com>
2023-07-19 10:39:23 +0530

    Initial commit

---
+++ implicit.java
@@ -0,0 +1,12 @@
+public class implicit
+{
+public static void main(String [] args)
+{
+int x=7;
+long y=x;
+float z=y;
+System.out.println("Befor the conversion of integer value:"+x);
+System.out.println("After the conversion of long value:"+y);
+System.out.println("After the conversion of float value:"+z);
+}
+}
